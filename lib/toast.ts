aa4d45e467d4990fee083c34fdd4c432907dfd28
Ayoola Abdulqudus <qqudusayo@gmail.com>
2025-03-23 17:54:39 +0100

    Refactor: enhance toast notification styles and add platform-specific top offset

--- lib/toast.ts
+++ lib/toast.ts
@@ -1,16 +1,17 @@
+import { Platform } from "react-native";
 import Toast, { ToastType } from "react-native-toast-message";
 
-export const toast = (
-  title: string,
-  msg: string,
-  type?: ToastType,
-) => {
+export const toast = (title: string, msg: string, type?: ToastType) => {
   return Toast.show({
     type: type || "success",
     swipeable: true,
     text1: title,
-    text1Style: { fontFamily: "AtkinsonHyperlegible" },
+    text1Style: { fontFamily: "AtkinsonHyperlegible", fontSize: 16 },
     text2: msg,
-    text2Style: { fontFamily: "AtkinsonHyperlegible" },
+    text2Style: { fontFamily: "AtkinsonHyperlegible", fontSize: 14 },
+    topOffset: Platform.select({
+      android: 60,
+      default: 40,
+    }),
   });
 };
